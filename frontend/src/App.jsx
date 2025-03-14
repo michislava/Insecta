242d862ae267c3a1262decd394312a02b04215c4
HP 830 G5 <mitkodd2005@gmail.com>
2025-03-14 22:54:16 +0200

    react router base

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,19 @@
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home/Home";
+import LoginPage from "./pages/Login/Login";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      Component: HomePage,
+      children: [
+        { path: "/", element: <HomePage /> },
+        { path: "login", element: <LoginPage /> },
+      ],
     },
   ]);
 
-  return <RouterProvider router={router}></RouterProvider>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
