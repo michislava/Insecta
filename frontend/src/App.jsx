122afa372ba35fc0c9906afe6d0104b3852b580f
Yosif Hamed <josif.hameed@gmail.com>
2025-03-15 10:18:13 +0200

    add: take a picture page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import HomePage from './pages/Home/Home'
 import Deck from './pages/Deck/Deck'
 import LoginPage from './pages/Login/Login'
+import ShootPage from "./pages/Shoot/Shoot";
+import RootPage from "./pages/Root/Root";
 
 function App() {
   const router = createBrowserRouter([
@@ -14,7 +16,11 @@ function App() {
         { path: 'deck', element: <Deck /> },
       ],
     },
-  ])
+    {
+      path: "/shoot",
+      element: <ShootPage />,
+    },
+  ]);
 
   return <RouterProvider router={router} />
 }
