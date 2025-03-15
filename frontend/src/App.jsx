57776e5e1df740508ce49dbaed2a1d838ccaf2af
HP 830 G5 <mitkodd2005@gmail.com>
2025-03-15 04:20:52 +0200

    updated home page : fe

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home/Home";
 import LoginPage from "./pages/Login/Login";
+import RootPage from "./pages/Root/Root";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
+      element: <RootPage />,
       children: [
         { path: "/", element: <HomePage /> },
         { path: "login", element: <LoginPage /> },
