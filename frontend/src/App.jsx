a48a9e831a6a3c4b8c999e599be366317d64c018
Yosif Hamed <josif.hameed@gmail.com>
2025-03-15 10:07:44 +0200

    fix: import root page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import HomePage from './pages/Home/Home'
 import Deck from './pages/Deck/Deck'
 import LoginPage from './pages/Login/Login'
+import RootPage from './pages/Root/Root'
 
 function App() {
   const router = createBrowserRouter([
