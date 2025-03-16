23d77ab6b711f38e17f86f3cbfbf4cf48bb32419
Yosif Hamed <josif.hameed@gmail.com>
2025-03-16 02:05:55 +0200

    add: animal in card requests

--- backend/db-services/cardService.ts
+++ backend/db-services/cardService.ts
@@ -8,7 +8,10 @@ export type CardPartial = Omit<Card, | 'id' | 'obtainmentDate'>;
 export async function getAllCardsForUser(userId: string): Promise<Card[]> {
     return await prisma.card.findMany({
         where: {
-            id: userId
+            ownerId: userId
+        },
+        include:{
+            animal: true
         }
     })
 }
@@ -17,6 +20,9 @@ export async function getCardById(cardId: string): Promise<Card | null> {
     return await prisma.card.findUnique({
         where: {
             id: cardId
+        },
+        include: {
+            animal:true
         }
     })
 }
