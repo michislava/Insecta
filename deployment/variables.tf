d0748626432935919e3629483d454e5d5512a900
echukaleva <emilia.chukaleva@itgix.com>
2025-03-16 11:17:27 +0200

    deployed resources

--- deployment/variables.tf
+++ deployment/variables.tf
@@ -1,13 +1,28 @@
-# variables.tf
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  default = "10.0.1.0/24"
+}
+
+variable "availability_zone" {
+  default = "us-east-1a"
+}
+
 
 variable "master_username" {
   description = "The master username for the Aurora PostgreSQL database"
   type        = string
-  sensitive   = true  # Marks the variable as sensitive (Terraform v0.14+)
+  sensitive   = true
 }
 
 variable "master_password" {
   description = "The master password for the Aurora PostgreSQL database"
   type        = string
-  sensitive   = true  # Marks the variable as sensitive (Terraform v0.14+)
+  sensitive   = true
 }
