a885e30712370e6dbdbedc63e630ec2a415e29af
HP 830 G5 <mitkodd2005@gmail.com>
2025-03-16 15:20:45 +0200

    set navigation links and cahnged logo

--- frontend/src/components/ActionMenu/ActionMenu.jsx
+++ frontend/src/components/ActionMenu/ActionMenu.jsx
@@ -1,51 +1,53 @@
-import classes from "./actionMenu.module.css";
+import classes from './actionMenu.module.css'
 
-import { NavLink } from "react-router-dom";
+import { NavLink } from 'react-router-dom'
 
 export default function ActionMenu() {
   return (
     <>
       <div className={classes.photoBtn}>
-        <i className="fa-solid fa-camera"></i>
+        <NavLink to='/shoot'>
+          <i className='fa-solid fa-camera'></i>
+        </NavLink>
       </div>
       <section className={classes.section}>
         <div>
           <NavLink
-            to="/"
+            to='/'
             className={({ isActive }) =>
               isActive ? classes.activeLink : classes.link
             }
           >
-            <i className="fa-solid fa-house"></i>
+            <i className='fa-solid fa-house'></i>
           </NavLink>
           <NavLink
-            to="/deck"
+            to='/deck'
             className={({ isActive }) =>
               isActive ? classes.activeLink : classes.link
             }
           >
-            <i className="fa-solid fa-bars-staggered"></i>
+            <i className='fa-solid fa-bars-staggered'></i>
           </NavLink>
         </div>
         <div>
           <NavLink
-            to="/history"
+            to='/history'
             className={({ isActive }) =>
               isActive ? classes.activeLink : classes.link
             }
           >
-            <i className="fa-solid fa-clock-rotate-left"></i>
+            <i className='fa-solid fa-clock-rotate-left'></i>
           </NavLink>
           <NavLink
-            to="/profile"
+            to='/profile'
             className={({ isActive }) =>
               isActive ? classes.activeLink : classes.link
             }
           >
-            <i className="fa-solid fa-user"></i>
+            <i className='fa-solid fa-user'></i>
           </NavLink>
         </div>
       </section>
     </>
-  );
+  )
 }
--- frontend/src/pages/Root/Root.jsx
+++ frontend/src/pages/Root/Root.jsx
@@ -1,18 +1,20 @@
-import classes from "./root.module.css";
+import classes from './root.module.css'
 
 // TODO: Change to logo
-import beeImg from "../../assets/images/home-hero.png";
+import logo from '../../assets/images/logo.png'
 
-import { Outlet } from "react-router";
+import { Outlet } from 'react-router'
 
 export default function RootPage() {
   return (
     <>
       <Outlet />
       <footer className={classes.footer}>
-        <img src={beeImg} alt="Logo" />
-        <i className="fa-brands fa-github"></i>
+        <img src={logo} alt='Logo' />
+        <a href='https://github.com/michislava/Insecta' target='_blank'>
+          <i className='fa-brands fa-github'></i>
+        </a>
       </footer>
     </>
-  );
+  )
 }
