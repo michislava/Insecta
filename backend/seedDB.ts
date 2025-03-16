66bca535834ae2a3bdd727522f236ff6425cf59d
Yosif Hamed <josif.hameed@gmail.com>
2025-03-16 02:50:58 +0200

    script: fix images in db seeding

--- backend/seedDB.ts
+++ backend/seedDB.ts
@@ -15,15 +15,15 @@ async function main(){
       
     const animals:any[] = await Promise.all([
         prisma.animal.create({data:{ latinName: 'Apis mellifera', name: 'Honeybee', description: 'Pollinating insect', animalClass: 'INSECTA' }}),
-        prisma.animal.create({data:{ latinName: 'Latrodectus mactans', name: 'Black Widow', description: 'Venomous spider', animalClass: 'ARACHNIDA' }}),
         prisma.animal.create({data:{ latinName: 'Acheta domesticus', name: 'House Cricket', description: 'Common cricket species', animalClass: 'INSECTA' }}),
+        prisma.animal.create({data:{ latinName: 'Latrodectus mactans', name: 'Black Widow', description: 'Venomous spider', animalClass: 'ARACHNIDA' }}),
     ])
 
     const cardsToAdd: any[] = [
         {
             "latitude": 40.7128,
             "longitude": -74.0060,
-            "pictureUrl": "https://example.com/image1.jpg",
+            "pictureUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/European_honey_bee_extracts_nectar.jpg/640px-European_honey_bee_extracts_nectar.jpg",
             "rarity": "COMMON",
             "animalId": animals[0].id,
             "ownerId": userIds[1-1],
@@ -33,7 +33,7 @@ async function main(){
         {
             "latitude": 34.0522,
             "longitude": -118.2437,
-            "pictureUrl": "https://example.com/image2.jpg",
+            "pictureUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Tonight_a_female_cricket_came_to_see_me%2C_but_he_did_not_..._told_me_%28Acheta_domestica%29-_DSC04475_by_Gianni_Del_Bufalo_%28bygdb%29_01.jpg/640px-Tonight_a_female_cricket_came_to_see_me%2C_but_he_did_not_..._told_me_%28Acheta_domestica%29-_DSC04475_by_Gianni_Del_Bufalo_%28bygdb%29_01.jpg",
             "rarity": "RARE",
             "animalId": animals[0].id,
             "ownerId": userIds[2-1],
@@ -43,7 +43,7 @@ async function main(){
         {
             "latitude": 37.7749,
             "longitude": -122.4194,
-            "pictureUrl": "https://example.com/image3.jpg",
+            "pictureUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_Widow_11-06.jpg/640px-Black_Widow_11-06.jpg",
             "rarity": "EPIC",
             "animalId": animals[0].id,
             "ownerId": userIds[3-1],
@@ -53,7 +53,7 @@ async function main(){
         {
             "latitude": 40.7306,
             "longitude": -73.9352,
-            "pictureUrl": "https://example.com/image4.jpg",
+            "pictureUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Apis_mellifera_flying.jpg/640px-Apis_mellifera_flying.jpg",
             "rarity": "COMMON",
             "animalId": animals[1].id,
             "ownerId": userIds[1-1],
@@ -63,7 +63,7 @@ async function main(){
         {
             "latitude": 34.0522,
             "longitude": -118.2437,
-            "pictureUrl": "https://example.com/image5.jpg",
+            "pictureUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Tonight_a_female_cricket_came_to_see_me%2C_but_he_did_not_..._told_me_%28Acheta_domestica%29-_DSC04471_by_Gianni_Del_Bufalo_%28bygdb%29_02.jpg/640px-Tonight_a_female_cricket_came_to_see_me%2C_but_he_did_not_..._told_me_%28Acheta_domestica%29-_DSC04471_by_Gianni_Del_Bufalo_%28bygdb%29_02.jpg",
             "rarity": "RARE",
             "animalId": animals[1].id,
             "ownerId": userIds[2-1],
@@ -73,7 +73,7 @@ async function main(){
         {
             "latitude": 37.7749,
             "longitude": -122.4194,
-            "pictureUrl": "https://example.com/image6.jpg",
+            "pictureUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Latrodectus.png/640px-Latrodectus.png",
             "rarity": "EPIC",
             "animalId": animals[1].id,
             "ownerId": userIds[3-1],
@@ -83,7 +83,7 @@ async function main(){
         {
             "latitude": 40.7306,
             "longitude": -73.9352,
-            "pictureUrl": "https://example.com/image7.jpg",
+            "pictureUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Bee_Collecting_Pollen_2004-08-14.jpg/640px-Bee_Collecting_Pollen_2004-08-14.jpg",
             "rarity": "COMMON",
             "animalId": animals[1-1].id,
             "ownerId": userIds[1-1],
@@ -93,7 +93,7 @@ async function main(){
         {
             "latitude": 34.0522,
             "longitude": -118.2437,
-            "pictureUrl": "https://example.com/image8.jpg",
+            "pictureUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Acheta-domestica-1.jpg/640px-Acheta-domestica-1.jpg",
             "rarity": "RARE",
             "animalId": animals[2-1].id,
             "ownerId": userIds[2-1],
@@ -103,7 +103,7 @@ async function main(){
         {
             "latitude": 37.7749,
             "longitude": -122.4194,
-            "pictureUrl": "https://example.com/image9.jpg",
+            "pictureUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Adult_Female_Black_Widow.jpg/640px-Adult_Female_Black_Widow.jpg",
             "rarity": "EPIC",
             "animalId": animals[3-1].id,
             "ownerId": userIds[3-1],
