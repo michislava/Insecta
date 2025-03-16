be3f62a0397e7fa4637a50673e3cb25cad3478c0
Yosif Hamed <josif.hameed@gmail.com>
2025-03-16 15:58:33 +0200

    remove: footer for shoot page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,9 +22,10 @@ function App() {
         { path: 'fight', element: <FightPage /> },
         { path: 'trade/:id?', element: <TradePage /> },
         { path: 'profile', element: <ProfilePage /> },
-        { path: 'shoot', element: <ShootPage /> },
+        // { path: 'shoot', element: <ShootPage /> },
       ],
     },
+    { path: 'shoot', element: <ShootPage /> },
   ])
 
   return <RouterProvider router={router} />
